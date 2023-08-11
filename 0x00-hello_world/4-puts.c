c077e602b1b43961f330de2dc6cbc640f22ff269
Sosphy <soskauria@gmail.com>
2023-08-10 17:12:36 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * main - this is the main funtion
- * 
- * Return: always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
