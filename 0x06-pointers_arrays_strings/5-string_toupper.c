2aa3d4518ad32e3d7f8e17dd4bbb88bbd1e9ce61
Sosphy <soskauria@gmail.com>
2023-08-24 19:50:22 -0700

    I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - function that changes all lowercase letters of a screen to uppercase letters
+ * @*: pointer to screen
+ * Return: pointer to uppercase string
+ */
+
+char *string_toupper(char *)
+{
+	int length_of_string;
+
+	length_of_string = 0;
+
+	while (*[length_of_string] != '\0'])
+	{
+		if (*[length_of_string] >= 97 && *[length_of_string] <= 122)
+		{
+			*[length_of_string] = *[length_of_string] - 32;
+		}
+		length_of_string++;
+	}
+	return (*)
+}
